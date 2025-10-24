b9887938f8d56d35247188ba96b3e03880207c66
lashakvaratskhelia777-blip <lasha.kvaratskhelia777@gmail.com>
2025-10-24 13:37:54 +0400

    second commit

---
+++ level03/homework/hw05.py
@@ -0,0 +1,15 @@
+#შექმენით შემდეგი ცვლადები snake_case გამოყენებით:
+
+my_name = "bacho"
+
+print(type("bacho"))
+
+
+"place_of_birth" = "tbilisi" 
+print(type("tbilisi"))
+
+"eye color" = "bronw"
+
+print(type("eye color"))
+
+
