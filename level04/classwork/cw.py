91498b0023acf23879929a61e8b64af35cb67505
lashakvaratskhelia777-blip <lasha.kvaratskhelia777@gmail.com>
2025-10-28 23:22:50 +0400

    second commit

---
+++ level04/classwork/cw.py
@@ -0,0 +1,14 @@
+ussr_name = "bacho"
+
+
+
+
+
+user_city = "tbilisi"
+
+user_name = input(ussr_name)
+
+print("hello" +  "ussr_name")
+ 
+user_city = input(user_city)
+print("hello" + "user_city")
---
+++ level04/homework/hw05.py
@@ -0,0 +1,28 @@
+#შექმენით 5 ცვლადი name,age,surname,city,hight.name-ში შეეკითხეს სახელი,age-ში ასაკი,surname-ში გვარი,city-ში ქალაქი ხოლო hight-ში სიმაღლე.ბოლოს გამოიტანეთ ეკრანზე ასეთი ინფორმაცია "მე მქვია (თქვენი სახელი), (თქვენი ასაკი) წლის,ვცხოვრობ (სადაც ცხოვრობთ) და ვარ (თქვენი სიმაღლე) სანდიმეტრი"
+
+
+name = input("ragqvia")
+age = input("ramdeni wlis xar")
+surname=input("ragvarixar")
+city= input("romel_qalaqsi_cxovrob")
+higty = input('ra_simagle_xar')
+
+
+
+print(name+ age+ surname+ city+ higty)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
